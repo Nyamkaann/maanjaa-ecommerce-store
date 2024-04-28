059fb1afabc894989165df30722d31fa2424e460
Nyamdorj <92709542+Nyamkaann@users.noreply.github.com>
2024-04-28 17:29:21 +0800

    patch v1
    2024 04 28

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="py-10 mx-auto">
                 <p className="text-xs text-center text-black">
-                    &copy; 2023 FakeStoreName, Inc. All rights reserved.
+                    &copy; 2024 Maanjaa
                 </p>
             </div>
         </footer>
